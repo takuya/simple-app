08f306a8451113e9008c8c1637f0e491fc31f579
takuya <takuya.1st+nospam@gmail.com>
2017-03-29 23:01:42 +0900

    /path/:name/user/:idのパラメータを取れるように

--- src/takuya/SimpleWebApp/SimpleRoutedWebApp.php
+++ src/takuya/SimpleWebApp/SimpleRoutedWebApp.php
@@ -19,6 +19,39 @@ class SimpleRoutedWebApp extends SimpleWebApp {
     self::$_instance = $this;
     $this->add_pre_get_filter( array($this,'rescan_hash_string_in_query_string') );
   }
+  protected function act_func($req_method){
+    $action = parent::act_func($req_method);
+    if (!$action){
+      return $this->default_routing($req_method);
+    }
+    return $action;
+  }
+
+  protected function default_routing($req_method){
+    $routes = $this->routes[$req_method];
+    $path = $this->act_name();
+    //マッチ対象を文字列の長さ順に並べておく.
+    $patterns = array_keys($routes);
+    array_multisort(array_map('strlen', $patterns),SORT_DESC, $patterns);
+    
+    foreach ($patterns as $key) {
+      $regex= preg_replace( '/:([^\/]+)/','(?<$1>[[:alnum:]]+)', $key );
+      $regex= preg_replace( '/\//','\\/', $regex);
+      $regex="/$regex/";
+      if( preg_match_all($regex,$path,$matches) ){
+        //TODO:: from 5.6 replace this foreach to array_filter
+        $matches = array_map(function($v){return $v[0];},$matches);
+        foreach ($matches as $k => $v) {
+          if (is_int($k)) { unset($matches[$k]);}
+        }
+        $func = $routes[$key];
+        return function() use ($func,$matches){
+           return call_user_func_array($func, [(object)$matches]);
+        };
+      }
+    }
+  }
+  
   public function act_name() {
     $request_path = parse_url($_SERVER['REQUEST_URI'])['path'];
     $path = str_replace($this->document_root,'' , $request_path);
--- tests/takuya/SimpleRoutedAppTest.php
+++ tests/takuya/SimpleRoutedAppTest.php
@@ -63,7 +63,61 @@ class SimpleRoutedWebAppTests extends \PHPUnit_Framework_TestCase {
    
    // 出力をテスト
    $this->expectOutputString('value');
-}
+  }
+  /**
+   * @covers Api::output
+   * @runInSeparateProcess
+   */
+  public function test_custom_routing_with_parameter(){
+    //ダミーリクエストデータ
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    $_SERVER['REQUEST_URI'] = '/~takuya/debug/12345?name=value';
+    parse_str( parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY) ,$a);
+
+    $_GET = $a;
+    $_POST = [];
+    $_REQUEST= array_merge( $_GET,$_POST );
+    
+    //メイン
+    $app = new SimpleRoutedWebApp();
+    $app->document_root = '/~takuya';
+    $app->get("/debug/:id" , function($args) use ($app){ 
+      echo $args->id;
+    });
+    
+    // 実行
+    $app->run();
+    
+    // 出力をテスト
+    $this->expectOutputString('12345');
+   }
+   /**
+    * @covers Api::output
+    * @runInSeparateProcess
+    */
+   public function test_custom_routing_with_multi_parameter(){
+     //ダミーリクエストデータ
+     $_SERVER['REQUEST_METHOD'] = 'GET';
+     $_SERVER['REQUEST_URI'] = '/~takuya/debug/12345/name/takuya/pref/hyogo?name=value';
+     parse_str( parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY) ,$a);
+
+     $_GET = $a;
+     $_POST = [];
+     $_REQUEST= array_merge( $_GET,$_POST );
+     
+     //メイン
+     $app = new SimpleRoutedWebApp();
+     $app->document_root = '/~takuya';
+     $app->get("/debug/:id/name/:name/pref/:pref" , function($args) use ($app){ 
+       echo $args->pref;
+     });
+     
+     // 実行
+     $app->run();
+     
+     // 出力をテスト
+     $this->expectOutputString('hyogo');
+    }
 
 }
 
